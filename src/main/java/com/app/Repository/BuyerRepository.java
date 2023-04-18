2363f0dfcb10dacc274c74e561f33937f4e48eae
Shawn <shawnyohannforrester@gmail.com>
2023-04-18 15:37:08 -0400

    cross origin statment updated

--- src/main/java/com/app/Repository/BuyerRepository.java
+++ src/main/java/com/app/Repository/BuyerRepository.java
@@ -1,6 +1,13 @@
 package com.app.Repository;
 import com.app.Model.Buyer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.RequestBody;
 
 public interface BuyerRepository extends JpaRepository<Buyer,Long> {
+
+    @Query("FROM Buyer WHERE username = :username")
+    Buyer getUserByUsername(@Param("username")String username);
+
 }
