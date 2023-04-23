2e5e744470733c9755be809ca27ca5a969307a9f
Tajah Daley <tajdaley@yahoo.comgit config --global user.name TajahDaley>
2023-04-23 12:38:02 -0500

    added inventory controller and servive

---
+++ src/main/java/com/app/Service/InventoryService.java
@@ -0,0 +1,26 @@
+package com.app.Service;
+
+import com.app.Repository.InventoryRepository;
+import com.app.Model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class InventoryService {
+    @Autowired
+    public InventoryService (InventoryRepository inventoryRepository) {this.inventoryRepository = inventoryRepository;}
+
+    InventoryRepository inventoryRepository;
+    public List<Product> getAllInventory() {
+        return inventoryRepository.findAll();}
+
+    public Product addNewInventory(Product product) {
+        return inventoryRepository.save(product);}
+
+    public Product deleteInventoryById() { return null;}
+
+    public List<Product> getProductByType(String type){
+        return inventoryRepository.findProductByType(type);
+    }
+}
